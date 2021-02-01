32e332e0acca32b373dc972ae9500b35c119e959
fcoardiles <77442132+fcoardiles@users.noreply.github.com>
2021-02-01 12:33:07 +0100

    change

--- WebSPA2/terminal-app/src/views/Login.vue
+++ WebSPA2/terminal-app/src/views/Login.vue
@@ -1,362 +1,253 @@
 <template>
-  <b-row
-    v-bind:style="{
-      backgroundImage: 'url(images/bk.webp)',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      height: '98vh',
-    }"
-  >
-    <b-col cols="12">
-      <b-row>
-        <b-col cols="12" class="text-left pb-2 pt-5 mt-3">
-          <h2
+  <b-container>
+    <video autoplay loop muted>
+      <source src="/video/bkn.webm" type="video/webm" />
+    </video>
+    <div id="filter-video"></div>
+    <div id="repeat"></div>
+    <div id="repeatH"></div>
+    <b-row
+      class="justify-content-center align-items-center"
+      v-bind:style="{ height: '90vh' }"
+    >
+      <div
+        align="center"
+        v-bind:style="{
+          backgroundColor: '#EAEBEE',
+          borderRadius: '5px',
+          zIndex: '20',
+          width: '420px',
+          paddingTop: '1em',
+        }"
+      >
+        <b-img src="/images/jp_logo.webp" height="50" center />
+        <h4
+          v-bind:style="{
+            fontFamily: 'Anton, sans-serif',
+            margin: '5px',
+            color: '#46536E',
+          }"
+        >
+          SAFE TERMINAL <i class="mdi mdi-lock-check" aria-hidden="true"></i>
+        </h4>
+        <div
+          v-bind:style="{
+            backgroundColor: '#D9912A',
+            marginTop: '1em',
+            width: '100%',
+            borderRadius: '0 0 5px 5px',
+          }"
+        >
+          <h5
+            class="pt-3 pb-1"
             v-bind:style="{
               fontFamily: 'Oswald, sans-serif',
-              backgroundColor: '#001D29',
-              padding: '5px 10px',
-              borderRadius: '5px',
-              color: 'white',
+              color: '#FFF',
             }"
           >
-            MUNKÁK
-          </h2>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col cols="12">
-          <b-progress height="2px" :value="timerProgress"></b-progress>
-          <b-table
-            hover
-            :items="loadData2"
-            :fields="fields"
-            sort-by="job.dateTime"
-            :sort-desc="true"
-            :tbody-tr-class="rowClass"
-            primary-key="job.id"
-            :no-provider-paging="true"
-            :no-provider-filtering="true"
-            :no-provider-sorting="true"
-            ref="table"
-            v-bind:style="{
-              textAlign: 'center',
-              backgroundColor: '#343A40',
-              color: 'white',
-            }"
-          >
-            <template v-slot:table-busy>
-              <div class="text-center text-danger my-2">
-                <b-spinner class="align-middle"></b-spinner>
-                <strong>{{ loadingText }}</strong>
-              </div>
-            </template>
-
-            <template v-slot:cell(datetime)="row">{{
-              new Date(Date.parse(row.item.job.dateTime)).toLocaleString()
-            }}</template>
-
-            <template v-slot:cell(action)="row">
-              <!--Kezdés-->
-              <ActionButton
-                v-if="row.item.job.jobStatus.id == 0"
-                :row="row"
-                :action="setState"
-                class="mdi mdi-calendar-start"
-                v-bind:style="{
-                  backgroundColor: 'transparent',
-                  color: '#57AB57',
-                  padding: 0,
-                  margin: 0,
-                  border: 'none',
-                }"
-                :ctx="{ statusId: 1 }"
-              />&nbsp;
-              <!--Megszakítás-->
-              <ActionButton
-                v-if="row.item.job.jobStatus.id == 1"
-                :row="row"
-                :action="setState"
-                class="mdi mdi-motion-pause"
-                v-bind:style="{
-                  backgroundColor: 'transparent',
-                  color: 'red',
-                  padding: 0,
-                  margin: 0,
-                  border: 'none',
-                }"
-                :ctx="{ statusId: -1 }"
-              />&nbsp;
-            </template>
-            <template v-slot:cell(action2)="row">
-              <!--Megnyitás-->
-              <ActionButton
-                :disabled="!row.item.vault"
-                :row="row"
-                :action="openClick"
-                class="mdi mdi-folder-download"
-                v-bind:style="{
-                  backgroundColor: 'transparent',
-                  color: '#37474F',
-                  padding: 0,
-                  margin: 0,
-                  border: 'none',
-                }"
-                :ctx="{}"
-              />
-            </template>
-          </b-table>
-        </b-col>
-      </b-row>
-    </b-col>
-  </b-row>
+            Helyezze be a kártyáját!
+          </h5>
+          <div class="mt-4 mb-2">
+            <b-icon
+              icon="credit-card"
+              variant="light"
+              animation="cylon"
+              scale="4"
+            ></b-icon>
+          </div>
+          <div class="pt-0 pb-4 text-light">
+            <b-spinner
+              v-for="i in 5"
+              :key="i"
+              style="width: 0.5rem; height: 0.5rem;"
+              label="Small Spinner"
+              class="mx-1"
+              type="grow"
+            ></b-spinner>
+          </div>
+        </div>
+      </div>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import axios from "axios";
+import Axios from "axios";
 export default {
   name: "Login",
-  components: {
-    ActionButton: () => import("../components/main/ActionButton"),
+  mounted() {
+    if (this.$store.reactive.hasSession) this.$router.push("/").catch(() => {});
   },
   async created() {
-    this.timer = setInterval(async () => {
-      if (this.actionInProgress) return;
-
-      this.timerProgress += 1;
-      if (this.timerProgress >= 100) {
-        this.timerProgress = 0;
-        this.$refs.table.refresh();
-      }
-    }, 500);
     this.$options.sockets.onmessage = (data) => {
       const event = JSON.parse(data.data);
-      //console.log(event);
-      if (event.Source == "BarcodeReader") {
-        this.handleBarcode(event.Message);
+      console.log(event);
+      if (event.Source == "CardReader") {
+        this.form.employeeCardCode = event.Message;
+        this.submit();
       }
     };
-  },
-  beforeDestroy() {
-    clearInterval(this.timer);
+
+    try {
+      this.loading = "Kapcsolódás az helyi ügynökhöz...";
+      const { data } = await this.$http.get("http://localhost:5100/terminalId");
+      this.test = 1;
+      this.$store.settings.setTerminalId(data.terminalId);
+      this.terminalId = data.terminalId;
+
+      this.terminalId = this.test;
+    } catch (error) {
+      if (error.response == undefined) {
+        // no agent
+      } else {
+        this.error.message = "" + error;
+      }
+
+      this.loading = null;
+      return;
+    }
+
+    try {
+      const { data } = await this.$http.get(
+        "api/management/session/ByTerminal/" +
+          this.$store.settings.getTerminalId() +
+          "?isActive=true"
+      );
+
+      if (data.terminalSessions.length < 1) {
+        this.loading = null;
+        return;
+      }
+
+      let sessions = data.terminalSessions.sort(function(a, b) {
+        return b.id - a.id;
+      });
+
+      this.$store.loadSession(sessions[0]);
+      this.$router.push("/").catch(() => {});
+    } catch (error) {
+      if (error.response.status == 404) {
+        // no session
+      } else {
+        this.error.message = "" + error;
+      }
+    }
+
+    this.loading = null;
   },
   data() {
     return {
-      loadingText: "Betöltés...",
-      actionInProgress: false,
-      timer: "",
-      timerProgress: 0,
-      fields: [
-        {
-          key: "job.id",
-          label: "ID",
-          tdClass: "align-middle",
-        },
-        {
-          key: "job.jobStatus.name",
-          label: "Státusz",
-          tdClass: "align-middle",
-        },
-        {
-          key: "job.notificationNumber",
-          label: "SAP jelentés",
-          tdClass: "align-middle",
-        },
-        {
-          key: "order.orderId",
-          label: "SAP Rendelés",
-          tdClass: "align-middle",
-        },
-        {
-          key: "vault.partNumber",
-          label: "Rajzszám",
-          tdClass: "align-middle",
-        },
-        {
-          key: "dateTime",
-          label: "Létrehozva",
-          tdClass: "align-middle",
-        },
-        {
-          key: "action",
-          label: "Művelet",
-          tdClass: "align-middle",
-        },
-        {
-          key: "action2",
-          label: "Megnyitás",
-          tdClass: "align-middle",
-        },
-      ],
-      //items: [],
-      loadData2: [
-        {
-          job: {
-            id: 21,
-            jobStatus: { name: "test" },
-          },
-          order: { orderId: 21 },
-          vault: { partNumber: 7507444 },
-          dateTime: "2020-10-06",
-        },
-      ],
+      form: {
+        employeeCardCode: "",
+        terminalId: "",
+      },
+      terminalId: null,
+      loading: null,
+      error: {
+        message: null,
+      },
+      readCardAnimation: "30",
     };
   },
   methods: {
-    async loadData() {
-      try {
-        const table = [];
-        this.loadingText = "Munkák betöltése...";
-        const { data } = await this.$http.get(
-          "api/management/job/byTerminal/" +
-            this.$store.settings.getTerminalId() +
-            "/dynamic"
-        );
-
-        data.terminalJobs.forEach((job, i) => {
-          table[i] = {
-            job,
-          };
-        });
-
-        var numbers = data.terminalJobs
-          .map((x) => "Numbers=" + x.notificationNumber)
-          .join("&");
-
-        if (numbers.length == 0) return [];
-
-        this.loadingText = "SAP adatok betöltése...";
-        const { data: sapData } = await this.$http.get(
-          "api/sap/getNotifications/byNotificationNumber?" + numbers
-        );
-
-        table.forEach((row) => {
-          row.order = sapData.notifications.find((n) =>
-            n.notificationNumber.endsWith("0000" + row.job.notificationNumber)
-          );
-        });
-
-        var orders = sapData.notifications
-          .map((x) => "sapOrders=" + x.orderId)
-          .join("&");
-
-        if (orders.length == 0) return [];
-
-        this.loadingText = "Vault adatok betöltése...";
-        const { data: vaultData } = await this.$http.get(
-          "api/vault/vaultLog/bySapOrder?" + orders
-        );
-
-        table.forEach((row) => {
-          row.vault = vaultData.vaultLogs.find(
-            (v) => v.sapOrder == row.order?.orderId ?? ""
-          );
-        });
-
-        return table;
-      } catch (error) {
-        this.displayError(error);
-        return [];
-      }
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.submit();
     },
-    rowClass(item, type) {
-      if (!item || type !== "row") return;
-      if (item.job.jobStatus.id == -1) return "bg-danger";
-      if (item.job.jobStatus.id == 1) return "bg-warning";
-      if (item.job.jobStatus.id == 2) return "bg-success";
-    },
-    async openClick(_, row) {
-      if (this.actionInProgress) return;
-      this.actionInProgress = true;
+    async submit() {
+      this.loading = "Betöltés...";
+      let terminalId = this.terminalId ?? this.form.terminalId;
 
       try {
-        const instance = axios.create();
-        const response = await instance.get(
-          "http://localhost:5100/DownloadAndOpenFileFromVault?fileName=" +
-            row.item.vault.fileName +
-            "&masterId=" +
-            row.item.vault.masterId,
-          { crossDomain: true }
-        );
-
-        this.actionInProgress = false;
-      } catch (error) {
-        if (error.response == undefined) {
-          // no agent
-          await this.download(row);
-          this.actionInProgress = false;
+        Axios.post("/api/management/session/", {
+          TerminalId: terminalId,
+          EmployeeCardCode: this.form.employeeCardCode,
+          CloseExistingSession: true,
+        });
+        /*
+        this.$store.loadSession(data.terminalSession);
+        this.$store.settings.setTerminalId(terminalId);
+        this.$router.push("/");*/
+      } catch (err) {
+        if (err.response.status == 404) {
+          this.error.message = "Ismeretlen terminál vagy kártyaszám!";
         } else {
-          this.displayError(error);
+          this.error.message = "" + err;
         }
-      }
-    },
-    async download(row) {
-      try {
-        const response = await this.$http.get(
-          "/api/vault/core/file/download/byMasterId/" + row.item.vault.masterId
-        );
 
-        var a = document.createElement("a");
-        a.href =
-          "data:application/document;base64," +
-          response.data.fileDownload.base64Content;
-        a.download = "" + row.item.vault.fileName;
-        a.click();
-        //window.location.href = "data:application/octet-stream;base64," + response.data.content;
-      } catch (error) {
-        this.displayError(error);
-      }
-    },
-    async setState(ctx, row) {
-      try {
-        await this.$http.put("/api/management/job/status", {
-          id: row.item.job.id,
-          statusId: ctx.statusId,
-        });
-      } catch (error) {
-        this.displayError(error);
-      }
-
-      this.$refs.table.refresh();
-    },
-    displayError(text) {
-      this.$bvToast.toast("" + text, {
-        title: `Hiba`,
-        variant: "danger",
-        solid: true,
-        toaster: "b-toaster-bottom-center",
-        appendToast: true,
-      });
-    },
-    handleBarcode(barcode) {
-      barcode = barcode.trim();
-      let row = this.$refs.table.localItems.find(
-        (r) =>
-          r.vault?.partNumber.trim() == barcode ||
-          r.job.notificationNumber == barcode
-      );
-
-      if (row == null) {
-        this.displayError("Nem található ilyen jelentés / rajz:" + barcode);
-      } else {
-        if (row.job.jobStatus.id == 0) {
-          this.setState({ statusId: 1 }, { item: row }); // To in-progress
-        }
-        /* else if (row.job.jobStatus.id == 1) {
-          this.setState({ statusId: 2 }, { item: row }); // To done
-        } */
+        return;
+      } finally {
+        this.loading = null;
       }
     },
   },
 };
 </script>
 <style>
-th {
-  font-weight: 800;
+video {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  background-size: 100% 100%;
+  background-color: black;
+  background-position: center center;
+  background-size: contain;
+  object-fit: cover;
+  z-index: 3;
 }
-tbody {
-  background-color: #ffffff !important;
-  color: #37474f;
-  font-family: "Oswald", sans-serif;
+#filter-video {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  background-color: rgba(0, 0, 27, 0.85);
+  z-index: 5;
+}
+#repeat {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  z-index: 6;
+  background-image: repeating-linear-gradient(
+    90deg,
+    rgba(0, 0, 0, 0.2) 8px 2px,
+    transparent 10px 20px
+  );
+}
+#repeatH {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 100%;
+  max-height: 100%;
+  z-index: 6;
+  background-image: repeating-linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.2) 8px 2px,
+    transparent 10px 20px
+  );
 }
 </style>
